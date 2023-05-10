7f7150c9be5f614a9246949cc0c0b0d9256f7043
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2023-05-11 04:21:30 +0800

    test

--- server.js
+++ server.js
@@ -40,12 +40,17 @@ app.post("/checkout", async (req, res) => {
 		});
 	});
 
+	const BASE_URL =
+		process.env.NODE_ENV === "development"
+			? "http://localhost:3000"
+			: "https://e-commerce-site-hazel.vercel.app";
+
 	const session = await stripe.checkout.sessions.create({
 		// payment_method_types: ["card"],
 		line_items: lineItems,
 		mode: "payment",
-		success_url: "https://e-commerce-site-hazel.vercel.app/success",
-		cancel_url: "https://e-commerce-site-hazel.vercel.app/cancel",
+		success_url: `https://e-commerce-site-hazel.vercel.app/success`,
+		cancel_url: `${BASE_URL}/cancel`,
 	});
 
 	res.send(JSON.stringify({ url: session.url }));
