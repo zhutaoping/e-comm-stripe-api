0b0a5426083660043481c991917aaf5d83407c11
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2023-05-12 22:22:24 +0800

    test

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -8,8 +8,8 @@ const corsOptions = {
 			callback(new Error("Not allowed by CORS"));
 		}
 	},
-	credentials: true,
-	optionsSuccessStatus: 200,
+	// credentials: true,
+	// optionsSuccessStatus: 200,
 };
 
 module.exports = corsOptions;
